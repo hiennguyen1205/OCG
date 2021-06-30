2e9de988353b40a2ca0ffef6d0a773ec0bee04f3
hiennguyen1205 <48577237+hiennguyen1205@users.noreply.github.com>
2021-06-30 14:44:29 +0700

    backend

---
+++ backend/routes/ConfigRouter.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/TechMaster/golang/15GoMySQL/controller"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ConfigRouter(app *fiber.App) {
+	manufacturerAPI := app.Group("/api/manufacturer")
+	routeManufacturer(&manufacturerAPI) //http://localhost:3000/api/book
+
+	productAPI := app.Group("/api/product")
+	routeProduct(&productAPI)
+}
+
+func routeManufacturer(router *fiber.Router) {
+	//Return all manufacturers
+	(*router).Get("/", controller.GetManufacturers) //Liệt kê
+}
+
+func routeProduct(router *fiber.Router) {
+	(*router).Get("/", controller.GetProducts) //Liệt kê
+
+	(*router).Get("/:id", controller.GetProductById)// tìm theo id
+
+	(*router).Get("/category/:name_category", controller.GetProductsByCategory) // tìm theo category
+}
